96c8d5203c441f645048ef3a3c8c91151af0cba0
Justin Barker <jstnbarker@pm.me>
2026-04-20 19:26:27 -0400

    Question object factory to reduce repetition

--- polls/tests.py
+++ polls/tests.py
@@ -6,6 +6,11 @@ import datetime
 from django.utils import timezone
 from .models import Question
 
+def create_question(question_text, days):
+    time = timezone.now() + datetime(timedelta(days=days))
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         time = timezone.now() + datetime.timedelta(days=30)
@@ -24,4 +29,5 @@ class QuestionModelTests(TestCase):
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
         return
+
 
