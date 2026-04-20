18ee99e34ee08b7d651ecd129ff743dd3be18d12
Justin Barker <jstnbarker@pm.me>
2026-04-20 19:31:43 -0400

    Remove unnecessary parenthesis

--- polls/views.py
+++ polls/views.py
@@ -10,7 +10,7 @@ class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "lastest_question_list"
     def get_queryset(self):
-        return Question.objects.filter((pub_date__lte=timezone.now())).order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
