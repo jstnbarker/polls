6fe3be371b13bb00fdac4093bb9181edf9572673
Justin Barker <jstnbarker@pm.me>
2026-04-20 19:33:24 -0400

    Utilize date creation function in existing tests

--- polls/tests.py
+++ polls/tests.py
@@ -7,27 +7,23 @@ from django.utils import timezone
 from .models import Question
 
 def create_question(question_text, days):
-    time = timezone.now() + datetime(timedelta(days=days))
+    time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
 
 
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
+        future_question = create_question("hello?", 30)
         self.assertIs(future_question.was_published_recently(), False)
-        return
 
     def test_was_published_recently_with_old_question(self):
-        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
-        old_question = Question(pub_date=time)
+        old_question = create_question("Old question?", 1.1)
         self.assertIs(old_question.was_published_recently(), False)
-        return
 
     def test_was_published_recently_with_recent_question(self):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
-        return
+
 
 
